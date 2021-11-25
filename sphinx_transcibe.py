3c3e21cd2c9c53cd09bbc88a67b9b5179f3d17c6
Can Bulguoglu <canbulguoglu@msn.com>
2021-11-25 11:06:14 +0300

    Transcription w Sphinx

---
+++ sphinx_transcibe.py
@@ -0,0 +1,105 @@
+'''
+================================================ 
+##            VOICEBOOK REPOSITORY            ##      
+================================================ 
+
+repository name: voicebook 
+repository version: 1.0 
+repository link: https://github.com/jim-schwoebel/voicebook 
+author: Jim Schwoebel 
+author contact: js@neurolex.co 
+description: a book and repo to get you started programming voice applications in Python - 10 chapters and 200+ scripts. 
+license category: opensource 
+license: Apache 2.0 license 
+organization name: NeuroLex Laboratories, Inc. 
+location: Seattle, WA 
+website: https://neurolex.ai 
+release date: 2018-09-28 
+
+This code (voicebook) is hereby released under a Apache 2.0 license license. 
+
+For more information, check out the license terms below. 
+
+================================================ 
+##               LICENSE TERMS                ##      
+================================================ 
+
+Copyright 2018 NeuroLex Laboratories, Inc. 
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+     http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License. 
+
+================================================ 
+##               SERVICE STATEMENT            ##        
+================================================ 
+
+If you are using the code written for a larger project, we are 
+happy to consult with you and help you with deployment. Our team 
+has >10 world experts in kafka distributed architectures, microservices 
+built on top of Node.JS / Python / Docker, and applying machine learning to 
+model speech and text data. 
+
+We have helped a wide variety of enterprises - small businesses, 
+researchers, enterprises, and/or independent developers. 
+
+If you would like to work with us let us know @ js@neurolex.co. 
+
+================================================ 
+##           SPHINX_TRANSCRIBE.PY             ##    
+================================================ 
+
+Transcribe audio with PocketSphinx.
+'''
+import speech_recognition as sr_audio
+import sounddevice as sd
+import soundfile as sf
+import os, json, datetime
+
+def sync_record(filename, duration, fs, channels):
+    print('recording')
+    myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=channels)
+    sd.wait()
+    sf.write(filename, myrecording, fs)
+    print('done recording')
+
+def transcribe_audio_sphinx(filename):
+    # transcribe the audio (note this is only done if a voice sample)
+    r=sr_audio.Recognizer()
+    with sr_audio.WavFile(filename) as source:
+        audio = r.record(source) 
+    text=r.recognize(audio)
+    print('transcript: '+text)
+    
+    return text
+
+def store_transcript(filename, transcript):
+    jsonfilename=filename[0:-4]+'.json'
+    print('saving %s to current directory'%(jsonfilename))
+    data = {
+        'date': str(datetime.datetime.now()),
+        'filename':filename,
+        'transcript':transcript,
+        }
+    print(data)
+    jsonfile=open(jsonfilename,'w')
+    json.dump(data,jsonfile)
+    jsonfile.close()
+
+# record file and print transcript
+filename='sync_record.wav'
+sync_record(filename, 3, 16000, 1)
+transcript=transcribe_audio_sphinx(filename)
+
+# now write the transcript into a .json file
+# e.g. sync_record.wav transcript will be stored in sync_record.json
+store_transcript(filename, transcript)
+
